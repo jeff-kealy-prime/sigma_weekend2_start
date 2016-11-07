f64e7fa9ba53bd2ff67309bd62d851e2abe0d322
Jeff Kealy <jeffkealy@MacBook-Pro-2.local>
2016-11-07 09:30:20 -0600

    final commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -22,29 +22,13 @@ $(document).ready(function(){
         error: function() {
           console.log('Error with request');
         }
-  /*
-  -One person's information
-
-  -A series of 22 (or the number of people in the data array)
-  index points with the first person's index highlighted or called out in
-  style differently than the others.
-  -A 'Next' button and a 'Previous' button
-  Clicking on the Next button should navigate to the next person, clicking
-  on the Previous button should navigate to the previous person. The highlighted
-  index point should update also as you click through to other people.
-
-  */
       });
   }
-
 });
-
-
-
 function appendToDom(){
   appendSingleObjInstance(data.sigmanauts[objectIndex])
-}
 
+}
 function addBlocks(){
   for (var i = 0; i < data.sigmanauts.length; i++) {
     $('#index').append('<div class="blocks" id="block' + i + '"></div>');
@@ -53,27 +37,23 @@ function addBlocks(){
     console.log("start" + objectIndex);
   }
 }
-
 function appendSingleObjInstance(person){
   $('#objectElements').animate({opacity: '0'}, 300).animate({opacity: '1'}, 300);
-  $('#objectElements').append('<div>' + person.name + '</div>')
-  $('#objectElements').append('<div>' + person.git_username + '</div>');
-  $('#objectElements').append('<div>' + person.shoutout + '</div>');
+  $('#objectElements').append('<div><b>Name:</b> ' + person.name + '</div>')
+  $('#objectElements').append('<div><b>Git Username:</b> ' + person.git_username + '</div>');
+  $('#objectElements').append('<div><b>Shoutout:</b> ' + person.shoutout + '</div>');
 }
-setInterval(nextButtonFunc, 10000);
+setTimeout(nextButtonFunc, 10000);
 function nextButtonFunc(){
-
   $('#objectElements').empty();
   objectIndex++;
   if(objectIndex > (data.sigmanauts.length-1)){
     objectIndex = data.sigmanauts.length-1;
   }
-  console.log(data.sigmanauts.length-1);
-  console.log(objectIndex);
-
   $('#block' + objectIndex).animate({opacity: '.1'}, 300).css({background : "black"}).animate({opacity: '1'}, 300);
   $('#block' + (objectIndex - 1)).css({background : "red"});
   appendToDom(objectIndex)
+  setTimeout(nextButtonFunc, 10000);
 
 }
 
@@ -86,6 +66,7 @@ function prevButtonFunc(){
   $('#block' + objectIndex).animate({opacity: '.1'}, 300).css({background : "black"}).animate({opacity: '1'}, 300);
   $('#block' + (objectIndex + 1)).css({background : "red"});
   appendToDom(objectIndex)
-  ;
+  setTimeout(nextButtonFunc, 10000);
+
 
 }
